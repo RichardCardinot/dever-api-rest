203b01b6144d284fefc76dc901b040631a057088
Richard Cardinot <rixa.git@gmail.com>
2022-10-07 22:23:23 -0300

    Subindo exercicio

--- dever/src/main/java/br/org/serratec/model/Cliente.java
+++ dever/src/main/java/br/org/serratec/model/Cliente.java
@@ -3,6 +3,7 @@ package br.org.serratec.model;
 import java.time.LocalDate;
 
 import javax.persistence.Column;
+import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -20,33 +21,35 @@ public class Cliente {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "id_cliente")
 	private Long id;
-	
+
 	@NotBlank(message = "O nome do cliente deve ser informado")
 	@Size(max = 60, min = 2, message = "O nome deve ter entre 2 a 60 caracteres")
 	@Column(length = 60)
 	private String nome;
-	
+
 	@NotBlank(message = "O CPF deve ser informado")
 	@Size(max = 11, min = 11, message = "O CPF deve ter 11 dígitos")
 	@Column(length = 11)
 	private String cpf;
-	
+
 	@NotBlank(message = "O email deve ser informado")
-	@Email(message = "O e-mail deve conter @ e ponto" )
+	@Email(message = "O e-mail deve conter @ e ponto")
 	@Size(max = 50, min = 10, message = "O email deve ter entre 10 a 50 caracteres")
 	@Column(length = 50)
 	private String email;
-	
 
 	@Past(message = "A nascimento deve ser igual ou menor que a data de hoje")
 	@NotNull(message = "A data de nascimento não pode ser nula")
 	@Column(name = "data_nascimento")
 	private LocalDate dataNascimento;
 
+	@Embedded
+	private Endereco endereço;
+
 	@Override
 	public String toString() {
 		return "Cliente [id=" + id + ", nome=" + nome + ", cpf=" + cpf + ", email=" + email + ", dataNascimento="
-				+ dataNascimento + "]";
+				+ dataNascimento + ", endereço=" + endereço + "]";
 	}
 
 	public String getNome() {
@@ -87,6 +90,14 @@ public class Cliente {
 
 	public void setId(Long id) {
 		this.id = id;
+	}
+
+	public Endereco getEndereço() {
+		return endereço;
+	}
+
+	public void setEndereço(Endereco endereço) {
+		this.endereço = endereço;
 	}
 
 }
